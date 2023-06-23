b5293e1e5e7d67734377b1ea114e285eb894be10
root <loaywaleed9@gmail.com>
2023-06-22 19:29:32 -0700

    header file name

--- more_stack_functions.c
+++ more_stack_functions.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 
 /**
  * _add - add top of stack y second top stack
--- stack_functions.c
+++ stack_functions.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 /**
  * _push - push int to a stack
  * @stack: linked lists for monty stack
